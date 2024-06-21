86e7f877a3223e7c82d63105ffbb8782cf84cfe2
Rishi Sharma <rishi.sharma2810@gmail.com>
2024-06-21 16:12:48 +0530

    All latest updates

--- app.py
+++ app.py
@@ -1,16 +1,124 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+from dotenv import load_dotenv
+import praw
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import string
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize NLTK resources
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Initialize PRAW with your Reddit API credentials
+reddit = praw.Reddit(client_id=os.getenv('REDDIT_CLIENT_ID'),
+                     client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+                     user_agent='my-app by u/Significant_Ad_4786')
+
+# Initialize the VADER sentiment analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+# Initialize NLTK's Snowball Stemmer and stopwords
+stemmer = SnowballStemmer('english')
+stop_words = set(stopwords.words('english'))
+
+# Function to perform sentiment analysis and visualization
+def analyze_and_visualize(subreddit_name, num_posts):
+    positive_sentiments = []
+    negative_sentiments = []
+    neutral_sentiments = []
+    top_positive_comment = ""
+    top_negative_comment = ""
+    top_neutral_comment = ""
+
+    for submission in reddit.subreddit(subreddit_name).new(limit=num_posts):
+        total_sentiment = 0
+        total_comments = 0
+        positive_comments = []
+        negative_comments = []
+        neutral_comments = []
+
+        for comment in submission.comments:
+            if isinstance(comment, praw.models.Comment):
+                comment_text = comment.body.lower()
+                comment_text = comment_text.translate(str.maketrans('', '', string.punctuation))
+                words = nltk.word_tokenize(comment_text)
+                words = [stemmer.stem(word) for word in words if word not in stop_words]
+                cleaned_comment = ' '.join(words)
+
+                sentiment = analyzer.polarity_scores(cleaned_comment)
+                total_sentiment += sentiment['compound']
+                total_comments += 1
+
+                if sentiment['compound'] >= 0.05:
+                    positive_comments.append(comment.body)
+                elif sentiment['compound'] <= -0.05:
+                    negative_comments.append(comment.body)
+                else:
+                    neutral_comments.append(comment.body)
+
+        if total_comments > 0:
+            average_sentiment = total_sentiment / total_comments
+            if average_sentiment >= 0.05:
+                positive_sentiments.append(average_sentiment)
+                top_positive_comment = get_top_comment(positive_comments, sentiment['compound'])
+            elif average_sentiment <= -0.05:
+                negative_sentiments.append(average_sentiment)
+                top_negative_comment = get_top_comment(negative_comments, sentiment['compound'])
+            else:
+                neutral_sentiments.append(average_sentiment)
+                top_neutral_comment = get_top_comment(neutral_comments, sentiment['compound'])
+
+    return (positive_sentiments, negative_sentiments, neutral_sentiments,
+            top_positive_comment, top_negative_comment, top_neutral_comment)
+
+
+# Function to get the top comment for each sentiment
+def get_top_comment(comments, sentiment):
+    if not comments:
+        return "No comments in this category."
+
+    # Find the comment with the highest positive or negative sentiment
+    top_comment = max(comments, key=lambda x: analyzer.polarity_scores(x)["compound"])
+
+    return top_comment
+
+
+@app.route('/', methods=['GET'])
+def home():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Handle form submission here
-    # Example: data = request.form['input_name']
-    return "Form submitted successfully!"
 
+@app.route('/redditSentiments', methods=[ 'POST'])
+def redditSentiments():
+    data = request.get_json()
+    print(data)
+    subreddit_name = data.get('subname')
+    num_posts = int(data['posts'])
+        
+    (positive_sentiments, negative_sentiments, neutral_sentiments,
+         top_positive_comment, top_negative_comment, top_neutral_comment) = analyze_and_visualize(subreddit_name, num_posts)
+
+    # print('Top Positive Comment',top_positive_comment)
+    # print(top_negative_comment)
+    # print(top_neutral_comment)
+    print(positive_sentiments)
+    print(negative_sentiments)
+    print(neutral_sentiments)
+
+    return  jsonify({
+        'comments': [top_positive_comment,top_negative_comment,top_neutral_comment],
+        'positive_sentiments':positive_sentiments,
+        'negative_sentiments':negative_sentiments,
+        'neutral_sentiments':neutral_sentiments
+    })
+                    
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,185 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // This function runs when the DOM is fully loaded
+
+  const commentsEl = document.getElementById("comments");
+  const canvasId = document.getElementById("canvas_id");
+  const submitBtn = document.getElementById("submit_button");
+  const subredditName = document.getElementById("subreddit_name");
+  const numPosts = document.getElementById("num_posts");
+  const typeOneBtns = document.querySelectorAll(".type-one-btn");
+  const typeTwoBtns = document.querySelectorAll(".type-two-btn");
+  let readMoreBtn = null;
+  let data = null;
+
+  // Define the fetchData function to handle form submission and fetch data from the server
+  const fetchData = async function (e) {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    try {
+      const inputData = {
+        subname: subredditName.value,
+        posts: parseInt(numPosts.value),
+      };
+
+      const response = await fetch("http://127.0.0.1:5000/redditSentiments", {
+        method: "post",
+        body: JSON.stringify(inputData),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      if (response.ok) {
+        data = await response.json(); // Parse JSON response
+        console.log(data);
+        canvasId.classList.remove("hidden");
+        canvasId.classList.add("flex");
+        updateChart(sentimentChart, data);
+        updateChart(sentimentChartTwo, data, "pie");
+        displayComments(data.comments);
+      } else {
+        console.error("Error fetching data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // Function to update the chart with the new data
+  const updateChart = function (chart, data, type = "bar") {
+    chart.config.type = type;
+    chart.data.datasets[0].data = [
+      data.positive_sentiments.length,
+      data.negative_sentiments.length,
+      data.neutral_sentiments.length,
+    ];
+    chart.update();
+  };
+
+  const displayComments = function (comments) {
+    const commentsColor = ["green", "red", "yellow"];
+    // commentsEl.innerHTML = "";
+    comments.forEach((comment, i) => {
+      const element = `
+        <div>
+            <p class="${commentsColor[i]} line-clamp-2">${comment}</p>
+            <button class="text-xs blue hover:underline" id="read-more-btn">Read More</button>
+        </div>`;
+      commentsEl.insertAdjacentHTML("beforeend", element);
+    });
+    readMoreBtn = document.querySelectorAll("#read-more-btn");
+
+    readMoreBtn.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        var content = btn.previousElementSibling;
+
+        if (content.classList.contains("expanded")) {
+          content.classList.remove("expanded");
+          content.classList.add("line-clamp-3");
+          btn.innerText = "Read More";
+        } else {
+          content.classList.remove("line-clamp-3");
+          content.classList.add("expanded");
+          btn.innerText = "Read Less";
+        }
+      });
+    });
+  };
+
+  typeOneBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const chartType = e.target.getAttribute("data-type");
+      updateChart(sentimentChart, data, chartType);
+    });
+  });
+
+  typeTwoBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const chartType = e.target.getAttribute("data-type");
+      updateChart(sentimentChartTwo, data, chartType);
+    });
+  });
+
+  submitBtn.addEventListener("click", fetchData);
+
+  // Initialize the Chart.js chart
+  var ctx = document.getElementById("sentimentChart1").getContext("2d");
+  var sentimentChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Positive", "Negative", "Neutral"],
+      datasets: [
+        {
+          label: "Sentiment Analysis",
+          data: [0, 0, 0],
+          backgroundColor: [
+            "rgba(54, 162, 235, 0.7)", // Blue
+            "rgba(255, 99, 132, 0.7)", // Red
+            "rgba(255, 206, 86, 0.7)", // Yellow
+          ],
+          borderColor: [
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(255, 206, 86, 1)",
+          ],
+          fill: true,
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      backgroundColor: "rgba(242, 242, 242, 0.2)", // Light gray background
+      scales: {
+        y: {
+          grid: {
+            color: "rgba(221, 221, 221, 0.5)", // Light gray grid lines
+            lineWidth: 0, // Adjust grid line thickness
+          },
+          beginAtZero: true,
+          responsive: true,
+        },
+      },
+    },
+  });
+
+  // type two chart initialization
+  var ctxTwo = document.getElementById("sentimentChart2").getContext("2d");
+  var sentimentChartTwo = new Chart(ctxTwo, {
+    type: "bar",
+    data: {
+      labels: ["Positive", "Negative", "Neutral"],
+      datasets: [
+        {
+          label: "Sentiment Analysis",
+          data: [0, 0, 0],
+          backgroundColor: [
+            "rgba(54, 162, 235, 0.7)", // Blue
+            "rgba(255, 99, 132, 0.7)", // Red
+            "rgba(255, 206, 86, 0.7)", // Yellow
+          ],
+          borderColor: [
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(255, 206, 86, 1)",
+          ],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      backgroundColor: "rgba(242, 242, 242, 0.2)", // Light gray background
+      scales: {
+        y: {
+          grid: {
+            color: "rgba(221, 221, 221, 0.5)", // Light gray grid lines
+            lineWidth: 0, // Adjust grid line thickness
+          },
+          beginAtZero: true,
+          responsive: true,
+        },
+      },
+    },
+  });
+});
