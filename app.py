caa9a770a996b0318389fb04637dd4c9380a0f79
Rishi Sharma <rishi.sharma2810@gmail.com>
2024-06-24 18:53:14 +0530

    Adding the stopwords as an array instead of downloading it from nltk dynamically

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import os
 import nltk
-
-# Set the NLTK data path to the current directory
-nltk.data.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# Rest of imports
 from flask import Flask, render_template, request, redirect, jsonify
 from dotenv import load_dotenv
 import praw
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 import string
 
@@ -28,7 +23,11 @@ analyzer = SentimentIntensityAnalyzer()
 
 # Initialize NLTK's Snowball Stemmer and stopwords
 stemmer = SnowballStemmer('english')
-stop_words = set(stopwords.words('english'))
+# stop_words = set(stopwords.words('english'))
+# Predefined list of stopwords
+STOP_WORDS = set([
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"
+])
 
 # Function to perform sentiment analysis and visualization
 def analyze_and_visualize(subreddit_name, num_posts):
@@ -51,7 +50,7 @@ def analyze_and_visualize(subreddit_name, num_posts):
                 comment_text = comment.body.lower()
                 comment_text = comment_text.translate(str.maketrans('', '', string.punctuation))
                 words = nltk.word_tokenize(comment_text)
-                words = [stemmer.stem(word) for word in words if word not in stop_words]
+                words = [stemmer.stem(word) for word in words if word not in STOP_WORDS]
                 cleaned_comment = ' '.join(words)
 
                 sentiment = analyzer.polarity_scores(cleaned_comment)
